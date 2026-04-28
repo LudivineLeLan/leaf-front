995ae7281f112ea7b69e2a4e5d6da785774b2add
LudivineLeLan <lelanludivine@gmail.com>
2026-04-28 09:26:09 +0200

    fix: axios with cookies only & not localStorage

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,15 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: 'http://localhost:3000',
-})
-
-// Ajoute automatiquement le token JWT à chaque requête si l'utilisateur est connecté
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token')
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-  return config
+  withCredentials: true, // ← envoie les cookies automatiquement
 })
 
 export default api
