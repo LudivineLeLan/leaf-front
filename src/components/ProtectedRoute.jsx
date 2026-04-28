89f9e2169f0ddfec0b55fbfaee2de6f1240bd275
LudivineLeLan <lelanludivine@gmail.com>
2026-04-28 09:28:22 +0200

    feat: auth context

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom'
 
 function ProtectedRoute({ children }) {
-  const token = localStorage.getItem('token')
+  const token = document.cookie.includes('token')
 
   if (!token) {
     return <Navigate to="/login" />
